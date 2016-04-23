41911919b79c43665909a67553d0d444a3ecd0e8
dprotopopov <protopopov@narod.ru>
2016-04-23 22:30:42 +0300

    222

--- Task02/memory.cpp
+++ Task02/memory.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> 
+#include <float.h>
 #include <mpi.h> 
 
 #ifndef max
@@ -14,10 +15,33 @@
 
 #define DATA_TAG 1
 
-int main(int argc, char *argv[])
+// искомая функция
+double func(int count, double* vi, double* ui)
 {
-	int nrank;     /* Общее количество процессов */
-	int myrank;    /* Номер текущего процесса */
+	double y = 0.0;
+	for (int i = 0; i < count; i++)
+	{
+		if (ui[i] < 0.001) return DBL_MAX ;
+		y += vi[i] / ui[i];
+	}
+	return y;
+}
+
+// функция проверки ограничений
+bool check(int count, double* ui, double v)
+{
+	double y = 0.0;
+	for (int i = 0; i < count; i++)
+	{
+		y += ui[i];
+	}
+	return y <= v;
+}
+
+int main(int argc, char* argv[])
+{
+	int nrank; /* Общее количество процессов */
+	int myrank; /* Номер текущего процесса */
 	MPI_Status status;
 
 	/* Иницилизация MPI */
@@ -25,40 +49,45 @@ int main(int argc, char *argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &nrank);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-	if (myrank == 0 && argc < 3){
-		printf("Usage :\t%s <inputfile> <outputfile>\n", argv[0]); fflush(stdout);
+	if (myrank == 0 && argc < 3)
+	{
+		printf("Usage :\t%s <inputfile> <outputfile>\n", argv[0]);
+		fflush(stdout);
 	}
 
-	if (argc < 3){
+	if (argc < 3)
+	{
 		MPI_Finalize();
 		exit(-1);
 	}
 
 	int count;
-	double v;
-	double *vi;
-	double *upperi;
-	double *loweri;
-	char *inputFileName;
-	char *outputFileName;
-	int i, j;
+	double v, y;
+	double* vi;
+	double* ui; // лучшее значение
+	double* xi; // перебираемое значение
+	double* upperi; // верхняя границы
+	double* loweri; // нижняя граница
+	char* inputFileName;
+	char* outputFileName;
 
 	// Получаем параметры - имена файлов
 	inputFileName = argv[1];
 	outputFileName = argv[2];
 
-	if (myrank == 0) {
-
+	if (myrank == 0)
+	{
 		printf("Input file name :\t%s\n", inputFileName);
 		printf("Output file name :\t%s\n", outputFileName);
 
 		/* Заполняем массив числами */
 		/* Операция выполняется только на ведущем процессе */
-		FILE *fl = fopen(inputFileName, "r");
+		FILE* fl = fopen(inputFileName, "r");
 		fscanf(fl, "%d", &count);
 		fscanf(fl, "%e", &v);
-		vi = static_cast<double *>(malloc(count*sizeof(double)));
-		for (i = 0; i<count; i++) {
+		vi = static_cast<double *>(malloc(count * sizeof(double)));
+		for (int i = 0; i < count; i++)
+		{
 			fscanf(fl, "%e", &vi[i]);
 		}
 		fclose(fl);
@@ -67,16 +96,82 @@ int main(int argc, char *argv[])
 	if (nrank > 1) MPI_Bcast(&count, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	if (nrank > 1) MPI_Bcast(&v, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	if (myrank > 0) vi = static_cast<double *>(malloc(count*sizeof(double)));
+	if (myrank > 0) vi = static_cast<double *>(malloc(count * sizeof(double)));
 	if (nrank > 1) MPI_Bcast(vi, count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	upperi = static_cast<double *>(malloc(count*sizeof(double)));
-	loweri = static_cast<double *>(malloc(count*sizeof(double)));
+	y = DBL_MAX ;
+	ui = static_cast<double *>(malloc(count * sizeof(double)));
+	xi = static_cast<double *>(malloc(count * sizeof(double)));
+	upperi = static_cast<double *>(malloc(count * sizeof(double)));
+	loweri = static_cast<double *>(malloc(count * sizeof(double)));
 
-	for (i = 0; i < count; i++) upperi[i] = v;
-	for (i = 0; i < count; i++) loweri[i] = 1.0/v;
+	for (int i = 0; i < count; i++) upperi[i] = v;
+	for (int i = 0; i < count; i++) loweri[i] = 0.0;
+
+	long total = 1L << (2 * count); // количество перебираемых точек
+
+
+	for (;;)
+	{
+		double distance = 0.0;
+		for (int i = 0; i < count; i++) distance = max(distance, upperi[i] - loweri[i]);
+		if (distance < 0.001) break;
+
+		for (long index = myrank * total / nrank; index < (myrank + 1) * total / nrank; index++)
+		{
+			long index2 = index;
+			for (int i = 0; i < count; i++)
+			{
+				int j = index2 % 4;
+				index2 /= 4;
+				xi[i] = loweri[i] + j * (upperi[i] - loweri[i]) / 3;
+			}
+			if (!check(count, xi, v)) continue;
+			double y2 = func(count, vi, xi);
+			if (y2 < y)
+			{
+				y = y2;
+				for (int i = 0; i < count; i++) ui[i] = xi[i];
+			}
+		}
+		if (myrank == 0)
+		{
+			for (int i = 1; i < nrank; i++)
+			{
+				double y2;
+				MPI_Recv(&y2, 1, MPI_DOUBLE, i, DATA_TAG, MPI_COMM_WORLD, &status);
+				MPI_Recv(xi, count, MPI_DOUBLE, i, DATA_TAG, MPI_COMM_WORLD, &status);
+				if (y2 < y)
+				{
+					y = y2;
+					for (int i = 0; i < count; i++) ui[i] = xi[i];
+				}
+			}
+			for (int i = 0; i < count; i++) loweri[i] = max(loweri[i], ui[i] - (upperi[i] - loweri[i]) / 3);
+			for (int i = 0; i < count; i++) upperi[i] = min(upperi[i], ui[i] + (upperi[i] - loweri[i]) / 3);
+		}
+		if (myrank > 0)
+		{
+			MPI_Send(&y, 1, MPI_DOUBLE, 0, DATA_TAG, MPI_COMM_WORLD);
+			MPI_Send(ui, count, MPI_DOUBLE, 0, DATA_TAG, MPI_COMM_WORLD);
+		}
+		MPI_Bcast(loweri, count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		MPI_Bcast(upperi, count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	}
+
+	/* выводим результаты */
+	if (myrank == 0) {
+		FILE *fl = fopen(outputFileName, "w");
+		for (int i = 0; i < count; i++) 
+		{
+			fprintf(fl, "%e\n", ui[i]);
+		}
+		fclose(fl);
+	}
 
 	free(vi);
+	free(xi);
+	free(ui);
 	free(upperi);
 	free(loweri);
 
