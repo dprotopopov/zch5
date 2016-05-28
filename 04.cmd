071c6051f608546d379945b0e6aa4f795c57f2f0
dprotopopov <protopopov@narod.ru>
2016-05-28 11:32:52 +0300

    04

--- 04.cmd
+++ 04.cmd
@@ -1,1 +1,1 @@
-Debug\Task04.exe Task04\input.txt Task04\output.txt
+"C:\Program Files\Microsoft MPI\Bin\mpiexec" -n 2 x64\Debug\Task04.exe Task04\input.txt Task04\output.txt
--- Task04/Task04.cpp
+++ Task04/Task04.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <numeric>
 #include <fstream>
+#include <mpi.h> 
+
+#define DATA_TAG 1
 
 // искомая функция
 double func(int count, std::vector<int>& vi, int v, std::vector<int>& ui)
@@ -24,6 +27,15 @@ double func(int count, std::vector<int>& vi, int v, std::vector<int>& ui)
 
 int main(int argc, char* argv[])
 {
+	int nrank; /* Общее количество процессов */
+	int myrank; /* Номер текущего процесса */
+	MPI_Status status;
+
+	/* Иницилизация MPI */
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
 	int count;
 	int v;
 	double y;
@@ -32,28 +44,49 @@ int main(int argc, char* argv[])
 	char* inputFileName;
 	char* outputFileName;
 
+	if (myrank == 0 && argc < 3)
+	{
+		printf("Usage :\t%s <inputfile> <outputfile>\n", argv[0]);
+		fflush(stdout);
+	}
+
+	if (argc < 3)
+	{
+		MPI_Finalize();
+		exit(-1);
+	}
+
 	// Получаем параметры - имена файлов
 	inputFileName = argv[1];
 	outputFileName = argv[2];
 
 	/* Заполняем массив числами */
-	std::ifstream ifs(inputFileName, std::ifstream::in);
-	ifs >> count;
-	ifs >> v;
-	for (int i = 0; i < count; i++)
+	if (myrank == 0)
 	{
-		int x;
-		ifs >> x;
-		vi.push_back(x);
+		std::ifstream ifs(inputFileName, std::ifstream::in);
+		ifs >> count;
+		ifs >> v;
+		for (int i = 0; i < count; i++)
+		{
+			int x;
+			ifs >> x;
+			vi.push_back(x);
+		}
+		ifs.close();
 	}
-	ifs.close();
+
+	if (nrank > 1) MPI_Bcast(&count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	if (nrank > 1) MPI_Bcast(&v, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	if (myrank > 0) vi.resize(count);
+	if (nrank > 1) MPI_Bcast(&vi[0], count, MPI_INT, 0, MPI_COMM_WORLD);
 
 	ui.push_back(v);
 	for (int i = 1; i < count; i++) ui.push_back(0);
 	y = func(count, vi, v, ui);
 	std::vector<int> xi(count);
 	std::copy(ui.begin(), ui.end(), xi.begin());
-	for (;;)
+	
+	for (int j = 0;;j%=nrank)
 	{
 		xi[xi.size() - 1]--;
 		if (xi[xi.size() - 1] == -1)
@@ -66,6 +99,8 @@ int main(int argc, char* argv[])
 		for (int i = xi.size(); i < count; i++)
 			xi.push_back(v - std::accumulate(xi.begin(), xi.end(), 0));
 
+		if (j++%nrank != myrank) continue;
+
 		double y1 = func(count, vi, v, xi);
 		if (y1 < y)
 		{
@@ -73,13 +108,33 @@ int main(int argc, char* argv[])
 			std::copy(xi.begin(), xi.end(), ui.begin());
 		}
 	}
-
-	std::ofstream ofs(outputFileName, std::ofstream::out);
-	for (int i = 0; i < count; i++)
+	if (myrank > 0)
 	{
-		ofs << ui[i] << '\n';
+		MPI_Send(&y, 1, MPI_DOUBLE, 0, DATA_TAG, MPI_COMM_WORLD);
+		MPI_Send(&ui[0], count, MPI_INT, 0, DATA_TAG, MPI_COMM_WORLD);
 	}
-	ofs.close();
 
+	xi.resize(count);
+	for (int i = 1; i < nrank;i++)
+	{
+		double y1;
+		MPI_Recv(&y1, 1, MPI_DOUBLE, i, DATA_TAG, MPI_COMM_WORLD, &status);
+		MPI_Recv(&xi[0], count, MPI_INT, i, DATA_TAG, MPI_COMM_WORLD, &status);
+		if (y1<y)
+		{
+			y = y1;
+			std::copy(xi.begin(), xi.end(), ui.begin());
+		}
+	}
+
+	if (myrank == 0)
+	{
+		std::ofstream ofs(outputFileName, std::ofstream::out);
+		for (int i = 0; i < count; i++)
+		{
+			ofs << ui[i] << '\n';
+		}
+		ofs.close();
+	}
 	return 0;
 }
